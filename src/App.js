f5c68e9c7d7b6f5e6e8646aae037b75c9bcbceee
esther615kim <75744588+esther615kim@users.noreply.github.com>
2021-09-28 00:10:14 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+//import "./styles.css";
+import React from "react";
+import Main from "./Main";
+// import Find from "./Find";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+}
