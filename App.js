10b08c1a1ab574ff2769d218f35feaab24526839
esther615kim <75744588+esther615kim@users.noreply.github.com>
2021-09-28 00:48:21 +0900

    added accordions components added accordions components

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 //import "./styles.css";
 import React from "react";
+import Find from "./src/components/Find";
 import Main from "./src/components/Main";
 
 export default function App() {
   return (
     <div className="App">
-      <Main />
+      {/* <Main /> */}
+      <Find />
     </div>
   );
 }
